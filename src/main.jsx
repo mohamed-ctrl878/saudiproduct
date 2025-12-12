6c507abbd156f8705e4708a749b82f85046a6e13
mohamed mahmoud <bnaboabdallah@gmail.com>
2025-12-12 07:45:12 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Analytics></Analytics>
       <App />
     </BrowserRouter>
   </StrictMode>,
